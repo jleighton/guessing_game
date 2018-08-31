8e69b13c0a3d5429a1c83894b13f7a65fb76a11c
* <github@jamesleighton.com>
2018-08-31 21:02:51 +0100

    Basic Logic, no error handling!

--- py-guess.py
+++ py-guess.py
@@ -1,3 +1,13 @@
 import random
 number = random.randrange(1,50)
 print (number)
+guess = int(input("Guess a number...."))
+
+while guess != number:
+    if guess > number:
+        guess = int(input("Lower...."))
+        
+    if guess < number:
+        guess = int(input("Higher...."))
+        
+print("Bingo!")
